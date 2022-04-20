8e75ca8e97bbb5b637452856bbdb545e504e9fa0
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2022-04-19 23:23:39 -0500

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import { useState } from 'react';
 import { Radio, Input } from 'antd';
 const Complex = require('complex.js');
 
+
+Math.Sin = function(w){
+  return parseFloat(Math.sin(w).toFixed(10));
+};
+
+Math.Cos = function(w){
+  return parseFloat(Math.cos(w).toFixed(10));
+};
+
 const raiz3 = Math.sqrt(3);
 const x10_3 = Math.pow(10, 3);
 const x10_6 = Math.pow(10, 6);
 
 
-const radToAng = (rad) => {
+function radToAng(rad) {
   return (rad * 180) / Math.PI;
 }
 
@@ -31,7 +40,9 @@ const getComplexPol = ({magnitude, angle}) => {
 
 const getComplexCart = ({real, img}) => {
   try {
-    return Complex(real, img ?? 0)
+    if(real || img){
+      return Complex(real || 0, img || 0)
+    }
   } catch (error) {
     console.log(error);
   }
@@ -41,9 +52,19 @@ const getComplexCart = ({real, img}) => {
 const calcIr = ({
   load,
   Fp,
-  Vr
+  Vr,
+  Is,
+  inpIrA,
+  inpIrV
 }) => {
-    if(load && Fp && Vr) {
+  if (inpIrV) {
+    return getComplexPol({
+      magnitude: parseFloat(inpIrV),
+      angle: angToRad(inpIrA || '0')
+    })
+  } else if(Is) {
+    return Is
+  } else if(load && Fp && Vr) {
       const magnitude = (load / (raiz3 * Vr * Fp) );
       const angle = -Math.acos(Fp) // en radianes y negativo dado que está en atraso
 
@@ -53,14 +74,19 @@ const calcIr = ({
   return ''
 }
 
-const calclVs = ({A,B,Vr,Ir}) => {
-  // Vs = AVr + BIr
-  if(A && B && Vr && Ir) {
+const calclVs = ({A,B,Vr,Ir, inpVsA, inpVsV}) => {
+  if(inpVsV){
+    return getComplexPol({
+      magnitude: parseFloat(inpVsV) * x10_3,
+      angle: angToRad(inpVsA || '0')
+    })
+  } else if(A && B && Vr && Ir) {
+    // Vs = AVr + BIr
+
     //A * Vr
-    const val1 = A.mul(Vr)
+    const val1 = A.mul((Vr/raiz3))
     // B * Ir
     const val2 = B.mul(Ir)
-    console.log(val1, val2);
     return val1.add(val2)
   }
   return ''
@@ -79,7 +105,7 @@ const calcA = ({line, inpAV, inpAA}) => {
       case 'corta':
         return Complex(1,0)
       case 'media':
-        break;
+        return ''
       case 'larga':
         break;
       default:
@@ -114,8 +140,7 @@ const calcB = ({line, inpBV, inpBA, Z}) => {
   return ''
 }
 
-
-const calcD = ({line, inpDV, inpDA, Z}) => {
+const calcD = ({line, inpDV, inpDA}) => {
   if(inpDV) {
     return getComplexPol({
       magnitude: parseFloat(inpDV),
@@ -139,48 +164,137 @@ const calcD = ({line, inpDV, inpDA, Z}) => {
   return ''
 }
 
+const calcDelta = ({ Vs, Vr, Ps, A, B, D, inpVsA}) => {
+  // valido si digitaron un angulo cuando Vs tiene valor
+  // en caso tal devuelvo el angulo ingresado
+  if (Vs && inpVsA) {
+    return inpVsA;
+  } else if (Ps && Vr && A && B && D) {
+    // Calculo el angulo a partir de la ecuación de Ps
+    // val1 = |D||Vs|^2 / |B| * cos(β - α)
+    const val1 = ((D.abs() * Math.pow(Vs.abs(), 2))/ B.abs()) * Math.Cos(B.arg() - A.arg())
+    // val2 = |Vs||Vr| / |B|
+    const val2 = (Vs.abs() * Vr)/ B.abs()
+    const val3 = (val1 - Ps) / val2;
+    // val4 = β - cos^-1 (val3)
+    const val4 = Math.acos(val3) - B.arg();
+    return radToAng(val4)
+  }
+  return ''
+};
+
+const calcPs = ({inpPs}) => {
+  if(inpPs) {
+    return parseFloat(inpPs)
+  }
+  return ''
+}
+
+const calcPr = ({inpPr}) => {
+  if(inpPr) {
+    return parseFloat(inpPr)
+  }
+  return ''
+}
+
+const calcQs = ({inpQs}) => {
+  if(inpQs) {
+    return parseFloat(inpQs)
+  }
+  return ''
+}
+
+const calcQr = ({inpQr}) => {
+  if(inpQr) {
+    return parseFloat(inpQr)
+  }
+  return ''
+}
+
+const calcIs = ({Ir, D, B, Vs, Vr}) => {
+  if (Ir) {
+    return Ir;
+  } else if(D && B && Vs && Vr) {
+    return (D.mul(Vs).sub(Vr)).div(B)
+  }
+  return ''
+}
+
+const calcVr = ({inpVrV, Vs, B, Ir, A}) => {
+  console.log(inpVrV, Vs, B, Ir, A);
+  if(inpVrV) {
+    return parseFloat(inpVrV) * x10_3
+  } else if (Vs && B && Ir && A) {
+    // Vr = (Vs - BIR ) / A
+    console.log('calcVr', (Vs.sub(B.mul(Ir))).div(A));
+    return ((Vs.sub(B.mul(Ir))).div(A)).abs()
+  }
+  return ''
+}
+
 function App() {
   // inputs
   const [line, setLine] = useState('corta');
   const [inpZR, setInpZR] = useState('');
   const [inpZI, setInpZI] = useState('');
   const [inpVsV, setInpVsV] = useState('');
-  const [inpVsA, setInpVsA] = useState('0');
+  const [inpVsA, setInpVsA] = useState('');
   const [inpVrV, setInpVrV] = useState('');
   const [inpAV, setInpAV] = useState('');
-  const [inpAA, setInpAA] = useState('0');
+  const [inpAA, setInpAA] = useState('');
   const [inpBV, setInpBV] = useState('');
-  const [inpBA, setInpBA] = useState('0');
+  const [inpBA, setInpBA] = useState('');
   const [inpDV, setInpDV] = useState('');
-  const [inpDA, setInpDA] = useState('0');
+  const [inpDA, setInpDA] = useState('');
   const [inpLoad, setInpLoad] = useState('');
-  const [inpFP, setInpFP] = useState('0');
-
-
+  const [inpFP, setInpFP] = useState('');
+  const [inpPr, setInpPr] = useState('');
+  const [inpQr, setInpQr] = useState('');
+  const [inpPs, setInpPs] = useState('');
+  const [inpQs, setInpQs] = useState('');
+  const [inpIrV, setInpIrV] = useState('');
+  const [inpIrA, setInpIrA] = useState('');
 
   // variables
+  let Ir, Is, Z, θ, A, B, D, Vs, Vr, Ps, Pr, δ, Qs, Qr;
+
   const load = parseFloat(inpLoad) * x10_6; // multiplico x10^6 para pasarlo a MV
   const Fp = parseFloat(inpFP);
-  const Vr = parseFloat(inpVrV) * x10_3;
 
-  const Z = (inpZR) ? getComplexCart({real: inpZR, img: inpZI}): '' // complejo
+  Ir = calcIr({load, Fp, Vr, Is, inpIrV, inpIrA}); // complejo
 
-  const Ir = calcIr({load, Fp, Vr});
+  Z =  getComplexCart({real: inpZR, img: inpZI}) // complejo
 
-  const A = calcA({line, inpAA, inpAV});
+  θ = Z ? radToAng(Z.arg()) : '';
 
-  const B = calcB({line, inpBA, inpBV, Z})
+  A = calcA({line, inpAA, inpAV}); // complejo
 
-  const D = calcD({line, inpDA, inpDV})
+  B = calcB({line, inpBA, inpBV, Z}) // complejo
 
-  const Vs = calclVs({A, B, Vr, Ir})
+  D = calcD({line, inpDA, inpDV}) // complejo
+
+  Vs = calclVs({A, B, Vr, Ir, inpVsA, inpVsV}) // complejo
+
+  Ps = calcPs({inpPs})
+
+  δ = calcDelta({Vs, Vr, inpVsA, Ps, A, B, D})
+
+  Pr = calcPr({inpPr})
+
+  Qs = calcQs({inpQs})
+
+  Qr = calcQr({inpQr})
+
+  Is = calcIs({Ir, D, B, Vr, Vs})
+
+  Vr = calcVr({inpVrV, Ir, B, Vs, A}); // real
 
   return (
     <div className="App">
       <h1>Sistemas de potencia</h1>
       <div className='sub-container'>
         <section>
-          <h2>Parametros de entrada</h2>
+          <h2>Parámetros de entrada</h2>
           <p>Tipos de línea</p>
           <Radio.Group onChange={(e) => {setLine(e.target.value)}} defaultValue="corta">
             <Radio.Button value="corta">Corta</Radio.Button>
@@ -199,10 +313,17 @@ function App() {
           <Input onChange={(e) => {setInpVsV(e.target.value)}} placeholder="100" />
           <span>Angulo (°)</span>
           <Input onChange={(e) => {setInpVsA(e.target.value)}} />
+          <p className='note'>Nota: El angulo debe estar vacio, si desea calcular: δ</p>
 
           <p>Vr</p>
           <span>Valor (KV)</span>
           <Input onChange={(e) => {setInpVrV(e.target.value)}} placeholder="100" />
+
+          <p>Ir</p>
+          <span>Valor (A)</span>
+          <Input onChange={(e) => {setInpIrV(e.target.value)}} placeholder="100" />
+          <span>Angulo (°)</span>
+          <Input onChange={(e) => {setInpIrA(e.target.value)}} />
 
           <p>A</p>
           <span>Valor</span>
@@ -222,6 +343,19 @@ function App() {
           <span>Angulo (°)</span>
           <Input onChange={(e) => {setInpDA(e.target.value)}} value={inpDA} />
 
+          <p>Potencia suministrada</p>
+          <span>Ps (W)</span>
+          <Input onChange={(e) => {setInpPs(e.target.value)}} placeholder="100" />
+          <span>Qs (VAR)</span>
+          <Input onChange={(e) => {setInpQs(e.target.value)}} placeholder="100" />
+
+          <p>Potencia recibida</p>
+          <span>Pr (W)</span>
+          <Input onChange={(e) => {setInpPr(e.target.value)}} placeholder="100" />
+          <span>Qr (VAR)</span>
+          <Input onChange={(e) => {setInpQr(e.target.value)}} placeholder="100" />
+
+
           <p>Potencia de la carga</p>
           <span>Valor (MV)</span>
           <Input onChange={(e) => {setInpLoad(e.target.value)}} placeholder="100" />
@@ -229,10 +363,15 @@ function App() {
           <Input onChange={(e) => {setInpFP(e.target.value)}} value={inpFP} />
         </section>
         <section>
-          <h2>Parametros de salida</h2>
-          {
+          <h2>Parámetros de salida</h2>
+          { // && = si
             !!Z && (
-              <p><strong>Z linea:</strong> {Z.toString()}</p>
+              <p><strong>Z:</strong> {Z.toString()}</p>
+            )
+          }
+          {
+            !!θ && (
+              <p><strong>θ:</strong> {θ.toString()}°</p>
             )
           }
           {
@@ -262,12 +401,51 @@ function App() {
           }
           {
             !!Ir && (
-              <p><strong>Ir:</strong> {`${Ir.abs().toFixed(4)} _/ ${radToAng(Ir.arg()).toFixed(4)} °`} A</p>
+              <p>
+                <strong>Ir:</strong> {`${Ir.abs().toFixed(4)}`}
+                <span className='angle'>{`${radToAng(Ir.arg()).toFixed(4)} °`}</span> A
+              </p>
             )
           }
           {
             !!Vs && (
-              <p><strong>Vs:</strong> {`${Vs.abs().toFixed(4)} _/ ${radToAng(Vs.arg()).toFixed(4)} °`} V</p>
+              <p>
+                <strong>Vs:</strong> {`${Vs.abs().toFixed(4)}`}
+                <span className='angle'>{`${radToAng(Vs.arg()).toFixed(4)} °`}</span> V
+              </p>
+            )
+          }
+          {
+            !!Is && (
+              <p>
+                <strong>Is:</strong> {`${Is.abs().toFixed(4)}`}
+                <span className='angle'>{`${radToAng(Is.arg()).toFixed(4)} °`}</span> A
+              </p>
+            )
+          }
+          {
+            !!Ps && (
+              <p><strong>Ps:</strong> {Ps.toString()} W</p>
+            )
+          }
+          {
+            !!δ && (
+              <p><strong>δ:</strong> {δ.toString()}°</p>
+            )
+          }
+          {
+            !!Qs && (
+              <p><strong>Qs:</strong> {Qs.toString()} VAR</p>
+            )
+          }
+          {
+            !!Pr && (
+              <p><strong>Pr:</strong> {Pr.toString()} W</p>
+            )
+          }
+          {
+            !!Qr && (
+              <p><strong>Qr:</strong> {Qr.toString()} VAR</p>
             )
           }
         </section>
